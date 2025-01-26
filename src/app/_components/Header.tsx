49cca0525c963535dd0e0dd8c1ce0707b1cefa76
Anton Amarjargal <anton@Antons-MacBook-Pro.local>
2025-01-26 21:45:39 +0800

    add

---
+++ src/app/_components/Header.tsx
@@ -0,0 +1,25 @@
+import { ChevronDown, Film } from "lucide-react";
+import { ModeToggle } from "../../components/ui/theme-toggle";
+
+export default async function Header() {
+  return (
+    <div className="flex items-center justify-center sticky top-0 left-0 right-0 ">
+      <div className="flex  ">
+        <img src="./film.svg" alt="" />
+        <p className="text-[#4338CA] italic font-bold">Movie Z</p>
+      </div>
+      <div className="flex">
+        <button className="flex items-center px-4 py-2 broder border-[#27272A] border-solid rounded-md gap-2">
+          <span className="text-14px font-medium">Genre</span>
+          <ChevronDown />
+        </button>
+        <input
+          type="text"
+          className=" px-3 py-2 bg-transparent text-[#A1A1A1] placeholder:text-[14px] rounded-lg "
+          placeholder="Search"
+        />
+      </div>
+      <ModeToggle />
+    </div>
+  );
+}
---
+++ src/app/_components/Popular.tsx
@@ -0,0 +1,27 @@
+import { Token } from "../_util/Constants.";
+import { MovieType } from "../_util/Type";
+import { Card } from "./Card";
+
+export const Popular = async () => {
+  const response = await fetch(
+    "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1",
+    {
+      headers: {
+        Authorization: `Bearer ${Token}`,
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  const data = await response.json();
+
+  return (
+    <div className="flex flex-col w-[1280px] m-auto  ">
+      <h1 className="text-[24px] font-semibold">Popular</h1>
+      <div className="flex flex-wrap  gap-5 justify-center  ">
+        {data.results?.map((movie: MovieType, index: number) => {
+          return <Card movie={movie} index={index} />;
+        })}
+      </div>
+    </div>
+  );
+};
